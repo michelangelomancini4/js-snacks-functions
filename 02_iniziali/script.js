69d7baa3aef728b2e59cff5eadc77e1e341d30cb
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-09 15:46:26 +0100

    consegna esercizio 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,15 +1,22 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const initials = getInitials(names);
-
-console.log(initials);
 
 // Dichiara la funzione qui.
 
+function genFirstLetter(namesArray) {
+
+    const firstLetter = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        
+        firstLetter[i] = namesArray[i][0];
+    }
+    return firstLetter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const initials = genFirstLetter(names);
+console.log(initials);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
