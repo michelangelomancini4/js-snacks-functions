8464d605997de3457c42bd63058757ca176ba203
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-09 14:53:28 +0100

    consegna esercizio 1

---
+++ 02_iniziali/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const initials = getInitials(names);
+
+console.log(initials);
+
+// Dichiara la funzione qui.
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
