c3461e3f967dab42e7f196c600c847e63fe9e576
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-10 16:04:37 +0100

    consegna esercizio 4 js-snacks-functions

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,24 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function vowelsCount (stringa) {
+    let vowels = 0;
 
+    // ciclo su stringa 
 
+    for (let i =0 ; i < stringa.length; i++) {
+
+        let carattere = stringa.charAt(i);
+
+        if(["a","e","i","o","u"].includes (carattere)) {
+            vowels += 1;
+        }
+    }
+    return vowels;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log("il numero delle vocali corrisponde a:", vowelsCount(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
